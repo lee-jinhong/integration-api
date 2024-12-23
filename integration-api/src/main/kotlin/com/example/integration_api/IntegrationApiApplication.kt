405110e8f6f2a67731d7b39aae5e1b94f3a5e827
이진홍 <jinhonglee@mustit.co.kr>
2024-12-23 15:06:54 +0900

    [main] 환경 세팅

---
+++ integration-api/src/main/kotlin/com/example/integration_api/IntegrationApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.integration_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntegrationApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntegrationApiApplication>(*args)
+}
